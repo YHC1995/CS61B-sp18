528f88696827a7b28c76d027a32097e8246762df
CYH <xdchen@chenxiaodongdeMacBook-Pro.local>
2022-04-15 23:43:06 +0800

    completed firt part of lab1

---
+++ lab1/HelloNumber.java
@@ -0,0 +1,9 @@
+public class HelloNumber{
+    public static void main(String[] args){
+        int s=0;
+        for(int i=0;i<10;i++){
+            s=s+i;
+            System.out.print(s + " ");
+        }
+    }
+}
